f0eb2a46c2df00429388a0dee7239f4f63f56fe6
moskensoap <141073988+moskensoap@users.noreply.github.com>
2025-06-02 17:41:09 +0000

    perf: append config.windows first to avoid window blinking

--- wezterm.lua
+++ wezterm.lua
@@ -12,10 +12,10 @@ require('events.tab-title').setup({ hide_active_tab_unseen = false, unseen_icon 
 require('events.new-tab-button').setup()
 
 return Config:init()
+   :append(require('config.windows'))
    :append(require('config.appearance'))
    :append(require('config.bindings'))
    :append(require('config.domains'))
    :append(require('config.fonts'))
    :append(require('config.general'))
-   :append(require('config.windows'))
    :append(require('config.launch')).options
