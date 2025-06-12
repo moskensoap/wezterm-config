c95889ed764371d473a638eedd50ec7df7f3f789
moskensoap <141073988+moskensoap@users.noreply.github.com>
2025-06-12 05:03:11 +0000

    feat: ssh add frp-archcraft

--- config/domains.lua
+++ config/domains.lua
@@ -45,6 +45,25 @@ return {
          multiplexing = "None",
 
          -- The name of this specific domain. Must be unique amongst
+         name = "frp-archcraft",
+
+         -- identifies the host:port pair of the remote server
+         -- Can be a DNS name or an IP address with an optional
+         -- ":port" on the end.
+         remote_address = "frp.archcraft.lan:22",
+
+         -- Whether agent auth should be disabled.
+         -- Set to true to disable it.
+         -- no_agent_auth = false,
+
+         -- The username to use for authenticating with the remote host
+         username = "root",
+      },
+      {
+         -- The connection is an ssh connection, don't use any multiplexing.
+         multiplexing = "None",
+
+         -- The name of this specific domain. Must be unique amongst
          name = "kalilinux",
 
          -- identifies the host:port pair of the remote server
