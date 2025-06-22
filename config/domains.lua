20b0c60cbf8ae0b82ceabd15fbf8ef46c9224e67
moskensoap <141073988+moskensoap@users.noreply.github.com>
2025-06-22 16:12:23 +0000

    feat(domains.lua): add ssh of docker-fedora and docker-gentoo

--- config/domains.lua
+++ config/domains.lua
@@ -192,6 +192,44 @@ return {
          -- The username to use for authenticating with the remote host
          username = "root",
       },
+      {
+         -- The connection is an ssh connection, don't use any multiplexing.
+         multiplexing = "None",
+
+         -- The name of this specific domain. Must be unique amongst
+         name = "docker-fedora",
+
+         -- identifies the host:port pair of the remote server
+         -- Can be a DNS name or an IP address with an optional
+         -- ":port" on the end.
+         remote_address = "192.168.2.47:22",
+
+         -- Whether agent auth should be disabled.
+         -- Set to true to disable it.
+         -- no_agent_auth = false,
+
+         -- The username to use for authenticating with the remote host
+         username = "root",
+      },
+      {
+         -- The connection is an ssh connection, don't use any multiplexing.
+         multiplexing = "None",
+
+         -- The name of this specific domain. Must be unique amongst
+         name = "docker-gentoo",
+
+         -- identifies the host:port pair of the remote server
+         -- Can be a DNS name or an IP address with an optional
+         -- ":port" on the end.
+         remote_address = "192.168.2.48:22",
+
+         -- Whether agent auth should be disabled.
+         -- Set to true to disable it.
+         -- no_agent_auth = false,
+
+         -- The username to use for authenticating with the remote host
+         username = "root",
+      },
    },
 
    -- ref: https://wezfurlong.org/wezterm/multiplexing.html#unix-domains
